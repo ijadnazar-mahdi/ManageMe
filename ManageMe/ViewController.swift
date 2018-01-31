61c39f0694039987064ed39da36c025f1eaabec4
Mehdi Ijadnazar <mehdiijadnazar@mehdis-macbook-pro.local>
2018-01-31 21:15:51 +0330

    Initial Commit

---
+++ ManageMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManageMe
+//
+//  Created by Mehdi Ijadnazar on 1/31/18.
+//  Copyright © 2018 Mehdi Ijadnazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
